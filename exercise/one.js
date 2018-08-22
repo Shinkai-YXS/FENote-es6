38f2d4eaa0ef9d3851add60eb67cd8e3ebbf3ed4
Yuxiaoshuang <1664226785@qq.com>
2018-08-22 11:35:22 +0800

    init

---
+++ exercise/one.js
@@ -0,0 +1,14 @@
+// 通过var定义的变量，作用域是整个封闭函数，是全域的。通过let定义的变量，作用域是在块级或是子块中.
+func();
+function func() {
+  if (true) {
+    var test = '🍎';
+  }
+  console.log(test);
+
+  if (true) {
+    let test1 = '🍊';
+  }
+  console.log(test1);
+}
+console.log(test);
