75d0aa80950eab9ccbc3766a826bdf2a110000fd
Yuxiaoshuang <1664226785@qq.com>
2018-08-23 10:19:28 +0800

    函数的名字-name属性

---
+++ exercise/函数的名字-name属性.js
@@ -0,0 +1,10 @@
+function breakfast() {}
+
+console.log(breakfast.name) // breakfast
+
+
+let test = function () {}
+console.log(test.name) // test
+
+let test1 = function superBreakfast() {}
+console.log(test1.name) // superBreakfast
