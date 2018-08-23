7d7385acb02eef3d9d543fa6cb9a74b0ccd73229
Yuxiaoshuang <1664226785@qq.com>
2018-08-23 09:52:42 +0800

    ... 的使用

--- exercise/展开操作符.js
+++ exercise/展开操作符.js
@@ -10,3 +10,20 @@ console.log(...fruits) // 🍎 🍌
 console.log(foods) // [ '🎃', '🍅', '🍆', '🌶', '🍎', '🍌' ]
 // 解构
 console.log(...foods) // 🎃 🍅 🍆 🌶 🍎 🍌
+
+
+// rest 操作符（一般用在函数的参数里头），参数数量不定时使用
+// ...foods 表示函数除了指定 dessert,drink 之外，其余参数的值都会放在一个叫 foods 的数组中
+function breakfast(dessert, drink, ...foods) {
+  console.log(dessert, drink, foods) // 🎂 ☕️ [ '🍊', '🍎', '🌰' ]
+  console.log(dessert, drink, ...foods) // 🎂 ☕️ 🍊 🍎 🌰
+}
+
+breakfast('🎂', '☕️', '🍊', '🍎', '🌰')
+
+
+// 解构参数
+function breakfast1(dessert, drink, {location, restaurant} = {}) {
+  console.log(dessert, drink, location, restaurant) // 🎂 ☕️ 成都 早餐店
+}
+breakfast1('🎂', '☕️', {location: '成都', restaurant: '早餐店'})
